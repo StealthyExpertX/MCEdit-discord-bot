cea15f440c3efafdd9932f3c6a9e29f2a6c2806e
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-10-04 20:48:05 -0400

    Rewrite for performance and regular expressions

--- reply.py
+++ reply.py
@@ -1,59 +1,59 @@
-# internal version 1.0.1
-# bot contributers -  naor2013, stealthyexpert, neonerz, gentlegiantjgc
+# bot contributers -  naor2013, stealthyexpert, neonerz, gentlegiantjgc, BluCode
 
 # required python imports
 import json
 import time
+from random import choice
+import re
 import discord
-from secrets import choice
+from discord_token import token
 
 # user response times cache
 users_answered_times = {}
 
-# load json data and return it either all the data or just the key.
-def loadJson(json_filename, json_key):
-	with open(json_filename) as json_file:
+class MCEdit(discord.Client):
+  def __init__(self, *args):
+    super().__init__(*args)
 
-		if json_key != None:
-			json_datas = json.load(json_file)
-			json_data = json.loads(json_datas)
-			return json_data[str(json_key)]
+    with open("blacklisted_roles.json") as f:
+      self.blacklisted_roles = set(json.load(f)["blacklisted_roles"])
+    with open("blacklisted_channels.json") as f:
+      self.blacklisted_channels = set(json.load(f)["blacklisted_channels"])
+    with open("answers.json") as f:
+      answers = json.load(f)
+    self.answers = {}
+    for question, answer in answers.items():
+      # 'questions' are space separated regular expressions which must
+      # all match somewhere in a message to flag it
+      compiled = tuple(re.compile(match) for match in question.split())
+      self.answers[compiled] = answer
 
-		json_datas = json.load(json_file)
-		json_data = json.loads(json_datas)
-		return json_data
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+    if message.channel.name in self.blacklisted_channels:
+      return
+    for role in message.author.roles:
+      if str(role) in self.blacklisted_roles:
+        return
+    # If the user was replied to less than 1 day ago, don't talk.
+    #if time.time() - users_answered_times.get(message.author, 0) < 24*3600:
+    #  return
 
-# client class where all the main code in running.
-class MyClient(discord.Client):
-	async def on_message(self, message):
+    # Loop through questions and check if user's message includes it.
+    for question in self.answers.keys():
+      for match in question:
+        res = match.search(message.content.lower())
+        if not res:
+          break
+      else:
+        channels = {ch.name: ch.mention for ch in message.guild.channels}
+        reply = choice(self.answers.get(question))
+        for name, mention in channels.items():
+          reply = reply.replace("{" + name + "}", mention)
+        await message.channel.send(reply)
+        users_answered_times[message.author] = time.time()
+        return
 
-		# Get the blacklisted roles list  from the blacklisted_roles.json.
-		blacklisted_roles = loadJson("blacklisted_roles.json", "blacklisted_roles")
-
-		# Get the blacklisted channels list from the blacklisted_channels.json.
-		blacklisted_channels = loadJson("blacklisted_channels.json", "blacklisted_channels")
-
-		answers = loadJson("answers", None)
-		questions = answers.keys()
-
-		# Loop through questions and check if user's message includes it.
-		for question in questions:
-			if message.author != self.user and question.lower() in message.content.lower():
-
-				# Blacklist any channels in the blacklisted_channels.json
-				if message.channel.name not in blacklisted_channels:
-					author_roles = list(map(str, message.author.roles))
-
-					# Blacklist any roles in the blacklisted_roles.json
-					for role in blacklisted_roles:
-						if role in author_roles:
-							return
-
-					# If the user was replied to more than 1 day ago, let the bot talk. 
-					if time.time() - users_answered_times.get(message.author, 0) > 24*3600:   
-						faq_channel = [ch for ch in message.guild.channels if ch.name == "faq"][0].mention
-						await message.channel.send(choice(answers.get(question)).format(faq=faq_channel))
-						users_answered_times[message.author] = time.time()
-#client code
-client = MyClient()
-client.run('XXXXXX')
+client = MCEdit()
+client.run(token)
